5bba7574483d328736907960d88d3f4a370f828d
Gentahall <gentahalilintar36@gmail.com>
2026-03-08 10:52:21 +0700

    firts commit for TP modul 3

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TP_MODUL3_103022400067;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
