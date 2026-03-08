6969030d004659971a14588889a68bb69e6d64e0
Gentahall <gentahalilintar36@gmail.com>
2026-03-08 11:15:22 +0700

    menambahkan implementasi kalkulator

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,42 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Data;
+using System.IO.Pipelines;
 
 namespace TP_MODUL3_103022400067;
 
 public partial class MainWindow : Window
 {
+
+    private string input = "";
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnButtonClick(object sender, RoutedEventArgs e)
+    {
+
+        var button = (Button)sender;
+        string key = button.Content.ToString();
+
+        if (key == "=")
+        {
+            try
+            {
+                var hasil = new DataTable().Compute(input, null);
+                LabelInput.Text = hasil.ToString();
+                input = hasil.ToString();
+            }
+            catch
+            {
+                LabelOutput.Text = "error";
+                input = "";
+            }
+        } else
+        {
+            input += key;
+            LabelOutput.Text = input;
+        }
     }
 }
--- obj/Debug/net10.0/TP_MODUL3_103022400067.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_MODUL3_103022400067.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL3_103022400067")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bba7574483d328736907960d88d3f4a370f828d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL3_103022400067")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL3_103022400067")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
